51ae6f992f348ef9122beb0ee52a3dcca93dc36c
youngkwon02 <youngkwon02@naver.com>
2022-08-12 00:07:41 +0900

    [DEPLOY] 2022.08.12

--- functions/api/routes/user/index.js
+++ functions/api/routes/user/index.js
@@ -1,8 +1,4 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/signup', require('./userSignupPOST'));
-router.post('/login', require('./userLoginPOST'));
-router.get('/nickname/check', require('./userNicknameCheckGET'));
-
 module.exports = router;
